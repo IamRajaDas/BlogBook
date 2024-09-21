05dbfa29cf7bc606f77cc61b0f56a19bed2f9e5d
Raja Das <rajadasraipur@gmail.com>
2024-09-21 23:28:24 +0530

    added theme switcher mode

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { Login as LoginComponent } from '../components'
 
 function Login() {
-  console.log("hii")
   return (
     <div className='py-8 mb-5'>
         <LoginComponent />
